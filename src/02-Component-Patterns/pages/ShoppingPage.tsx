7969ad7d707837d154f195803a7b8b1c90fcd586
Mariano De Simone <marianodesimone@Marianos-MacBook-Air.local>
2022-03-28 14:12:12 +0200

    Component Extend Styles

--- src/02-Component-Patterns/pages/ShoppingPage.tsx
+++ src/02-Component-Patterns/pages/ShoppingPage.tsx
@@ -5,6 +5,8 @@ import {
   ProductTitle,
 } from '../components';
 
+import '../styles/custom-styles.css';
+
 const product = {
   id: '1',
   title: 'Coffe Mug - Card',
@@ -18,17 +20,27 @@ export default function h1() {
       <hr />
       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
         <ProductCard product={product}>
-          <ProductCard.Image />
-          <ProductCard.Title title={'Taza de café'} />
-          <ProductCard.Buttons />
+          <ProductCard.Image className="custom-image" />
+          <ProductCard.Title className="text-bold" title={'Taza de café'} />
+          <ProductCard.Buttons className="custom-buttons" />
         </ProductCard>
-      </div>
 
-      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-        <ProductCard product={product}>
-          <ProductImage />
-          <ProductTitle />
-          <ProductButtons />
+        <ProductCard product={product} className="bg-dark text-white ">
+          <ProductImage className="custom-image" img={''} />
+          <ProductTitle className="text-bold" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
+
+        <ProductCard
+          product={product}
+          className="bg-dark text-white"
+          style={{ backgroundColor: '#70d1f8' }}
+        >
+          <ProductImage style={{ fontWeight: 'bold' }} />
+          <ProductTitle
+            style={{ boxShadow: '10px 10px 10px rgba(0,0,0, 0.2' }}
+          />
+          <ProductButtons style={{ display: 'flex', justifyContent: 'end' }} />
         </ProductCard>
       </div>
     </div>
